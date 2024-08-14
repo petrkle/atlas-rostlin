3e1a6ff401213e73f4bcdddb265020fa06a84110
Petr <petr@kle.cz>
2024-08-14 22:11:59 +0200

    Cordova 13

--- app/src/main/java/cz/kle/atlasrostlin/AtlasRostlin.java
+++ app/src/main/java/cz/kle/atlasrostlin/AtlasRostlin.java
@@ -3,17 +3,17 @@ package cz.kle.atlasrostlin;
 import android.os.Bundle;
 import org.apache.cordova.*;
 
-public class AtlasRostlin extends CordovaActivity 
+public class AtlasRostlin extends CordovaActivity
 {
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-				Bundle extras = getIntent().getExtras();
+        Bundle extras = getIntent().getExtras();
         if (extras != null && extras.getBoolean("cdvStartInBackground", false)) {
             moveTaskToBack(true);
         }
         loadUrl(launchUrl);
-				appView.getView().setVerticalScrollBarEnabled(true);
+        appView.getView().setVerticalScrollBarEnabled(true);
     }
 }
